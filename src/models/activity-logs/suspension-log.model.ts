3a33f5e526f243ab811c33e46fece251e82e2290
philipthedeveloper <philipowolabi79@gmail.com>
2025-07-08 14:13:10 +0100

    Update user suspension model

--- src/models/activity-logs/suspension-log.model.ts
+++ src/models/activity-logs/suspension-log.model.ts
@@ -12,7 +12,39 @@ export interface ISuspensionLog extends Document<Types.ObjectId> {
 }
 
 const SuspensionLogSchema: Schema<ISuspensionLog> = new Schema<ISuspensionLog>(
-  {},
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: [true, "Please provide user id"],
+      ref: "users",
+    },
+    role: {
+      type: String,
+      required: [true, "Please provide user role"],
+      enum: { values: Object.values(UserTypes), message: "Unsupported role" },
+    },
+    suspensionDate: {
+      type: Date,
+      default: Date.now,
+      required: [true, "Please provide suspension date"],
+    },
+    suspendedBy: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "admins",
+    },
+    reason: {
+      type: String,
+      required: [true, "Please provide suspension reason"],
+    },
+    liftedOn: {
+      type: Date,
+    },
+    liftedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "admins",
+    },
+  },
   {
     autoIndex: true,
     timestamps: true,
