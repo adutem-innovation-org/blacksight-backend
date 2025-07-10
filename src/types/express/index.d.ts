36142cb25c62acf415ef3a718960d00ff706e8f4
philipthedeveloper <philipowolabi79@gmail.com>
2025-07-10 22:56:18 +0100

    ⬆️[UPDATE]: Add new field to extended express request type

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -3,5 +3,7 @@ import { AuthData } from "./src/interfaces/auth/auth-data";
 declare module "express-serve-static-core" {
   interface Request {
     authData?: AuthData;
+    sessionId?: string;
+    apiKeyOwnerId?: string;
   }
 }
