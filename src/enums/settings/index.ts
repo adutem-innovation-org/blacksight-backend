0ff60306d3536e7afefade850cfe201169115c80
philipthedeveloper <philipowolabi79@gmail.com>
2025-07-19 20:38:32 +0100

    ⬆️[UPDATE]: Settings keys update

--- src/enums/settings/index.ts
+++ src/enums/settings/index.ts
@@ -11,4 +11,13 @@ export enum SettingsKeys {
   MARK_UP_PERCENT = "markUpPercent",
   COST_PER_STORAGE_GB = "costPerStorageGB",
   STORAGE_MARK_UP_PERCENT = "storageMarkUpPercent",
+  TOKEN_CONVERSATION_FACTOR = "tokenConversionFactor",
+  COST_PER_PROMPT_TOKEN = "costPerPromptToken",
+  COST_PER_CACHED_PROMPT_TOKEN = "costPerCachedPromptToken",
+  COST_PER_COMPLETION_TOKEN = "costPerCompletionToken",
+  CHAT_COMPLETION_MARK_UP_PERCENT = "chatCompletionMarkUpPercent",
+  COST_PER_EMBEDDING_TOKEN = "costPerEmbeddingToken",
+  EMBEDDINGS_MARK_UP_PERCENT = "embeddingsMarkUpPercent",
+  COST_PER_TRANSCRIPTION_MINUTE = "costPerTranscriptionMinute",
+  TRANSCRIPTION_MARK_UP_PERCENT = "transcriptionMarkUpPercent",
 }
