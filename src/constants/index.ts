ba9f1b9da39f0ae72ac37d2d0cc2ed8bd2918b23
philipthedeveloper <philipowolabi79@gmail.com>
2025-07-30 15:45:19 +0100

    ⬆️[UPDATE]: Declare dynamic template field constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export * from "./auth";
 export * from "./bot";
 export * from "./storage";
+export * from "./templates"
---
+++ src/constants/templates/index.ts
@@ -0,0 +1,14 @@
+export const APPOINTMENT_TEMPLATE_DYNAMIC_FIELDS = [
+    "customerEmail",
+    "customerName",
+    "customerPhone",
+    "appointmentDate",
+    "appointmentTime"
+]
+
+export const PAYMENT_TEMPLATE_DYNAMIC_FIELDS = [
+    "customerName",
+    "customerEmail",
+    "amount",
+    "plan"
+]
