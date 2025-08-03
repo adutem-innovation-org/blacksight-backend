18e5cbac0f7a511297f6150524a8f9e74f4fdc4a
philipthedeveloper <philipowolabi79@gmail.com>
2025-08-03 23:56:25 +0100

    ⬆️[UPDATE]: Add new durations to TTL enums used in cache service

--- src/enums/ttl/index.ts
+++ src/enums/ttl/index.ts
@@ -1,9 +1,16 @@
+/*
+ * @file TTL
+ * @description Time to live for cache and session (seconds)
+ */
+
 export enum TTL {
   IN_A_MINUTE = 60,
   IN_5_MINUTES = 5 * 60,
   IN_10_MINUTES = 10 * 60,
+  IN_15_MINUTES = 15 * 60,
   IN_30_MINUTES = 30 * 60,
   IN_AN_HOUR = 60 * 60,
   IN_2_HOURS = 2 * 60 * 60,
   IN_3_HOURS = 3 * 60 * 60,
+  IN_24_HOURS = 24 * 60 * 60,
 }
